e1eda8f278a5bbde784fa89598db9be25ee67808
martin gimenez <jm.gimenez.inf@gmail.com>
2018-06-04 19:22:17 -0300

    código refactorizado

--- pavaTemperaturaRelayWithWIFI/pavaTemperaturaRelayWithWIFI.ino
+++ pavaTemperaturaRelayWithWIFI/pavaTemperaturaRelayWithWIFI.ino
@@ -3,68 +3,71 @@
 #include <DallasTemperature.h>
 
 long inputTemperatura,tempActual;
-char buf[50];
-int pinRelay=3;
-int pinTemp=2;
-OneWire ourWire(pinTemp);                //Se establece el pin 2  como bus OneWire
-DallasTemperature sensorTemperatura(&ourWire); //Se declara una variable u objeto para nuestro sensor1
-SoftwareSerial mySerial(13,12); // RX, TX nodemcu 
+// Constantes
+const int pinRelay=3;
+const int pinTemp=2;
+const int pinNodeMCU=12;
+
+OneWire ourWire(pinTemp);                      //Se establece el pin 2  como bus OneWire
+DallasTemperature sensorTemperatura(&ourWire); //Se declara una variable u objeto para el sensor de temperatura
+SoftwareSerial serialNodeMcu(13,12);           //RX, TX nodemcu 
+
 void setup()
 {
- //config serial nodemcu 
- mySerial.begin(115200);
- pinMode(12,OUTPUT); 
- 
+ //configuración de pines 
+ pinMode(pinNodeMCU,OUTPUT); 
  pinMode(pinRelay,OUTPUT) ;
+ 
+ //inicialización de puertos seriales
  Serial.begin(115200);
- digitalWrite(pinRelay, HIGH);
- sensorTemperatura.begin();   //Se inicia el sensor 1
+ serialNodeMcu.begin(115200);
+ 
+ //inicialización de sensores y actuador
+ sensorTemperatura.begin();                   //Se inicia el sensor de temperatura
+ digitalWrite(pinRelay, HIGH);                //iniciamos el relay en apagado
 }
 
-char mystr[10];
-void loop(){
+
+void loop()
+{
   inputTemperatura = 0;
   Serial.println("Esperando conectar serial wifi");
-  while(!mySerial){  
+  while(!serialNodeMcu){  
   }
-  //Espero a leer una temperatura por puerto serial 
-  Serial.println("Esperando iniciar <Presione una tecla>");
+  
+  //Espero a leer una temperatura desde NodeMCU
+  Serial.println("Esperando solicitud de calentar");
   while(inputTemperatura == 0){
-    // mySerial.readBytes(mystr,2); //Read the serial data and store in var
-    // Serial.println(mystr);
-
-    if (mySerial.available()>0){
-     inputTemperatura = mySerial.parseInt();
-     mySerial.flush();
+    if (serialNodeMcu.available()>0){
+     inputTemperatura = serialNodeMcu.parseInt();
+     serialNodeMcu.flush();
+     Serial.println("Temperatura recibida: ")
+     Serial.println(inputTemperatura);
     }
     delay(500);
-    Serial.println(inputTemperatura);
-    
   }
+
   //Envio ok se leyo la temperatura 
-  Serial.print(inputTemperatura);
   Serial.println("Listo");  
+  
   //Enciendo la pava
   digitalWrite(pinRelay, LOW);
   
-  sensorTemperatura.requestTemperatures();   //Se envía el comando para leer la temperatura
-  tempActual= sensorTemperatura.getTempCByIndex(0); //Se obtiene la temperatura en ºC del sensor
-  Serial.print(tempActual);
-  //sensar temperatura
-    Serial.print("Calentando");
+  //Sensar temperatura hasta alcanzar la temperatura deseada
+  Serial.print("Calentando");
   while(tempActual < inputTemperatura){
-    sensorTemperatura.requestTemperatures();   //Se envía el comando para leer la temperatura
-    tempActual= sensorTemperatura.getTempCByIndex(0); //Se obtiene la temperatura en ºC del sensor
-     Serial.print("Temperatura actual= ");
-     Serial.print(tempActual);
-     Serial.println(" C");
-     delay(10);   
+    sensorTemperatura.requestTemperatures();            //Se envía el comando para leer la temperatura
+    tempActual= sensorTemperatura.getTempCByIndex(0);   //Se obtiene la temperatura en ºC del sensor
+    Serial.print("Temperatura actual= ");
+    Serial.print(tempActual);
+    Serial.println(" C");
+    delay(10);   
   }
+
   //Apago la pava
   digitalWrite(pinRelay,HIGH);
   //Envío finalizo
-  Serial.println("Finalizo"); 
-    
+  Serial.println("Finalizo");   
 }
 
 
